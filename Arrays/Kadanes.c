ab8506e14cedd9f5b15bcfc1e6b283f70cabf3c6
Priyanshu <singhpriyanshu608@gmail.com>
2025-10-25 22:52:48 +0530

    arrays

---
+++ Arrays/Kadanes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n = 5;
+    int arr[5] = {1,2,3,4,5};
+    for(int str = 0; str < n; str++){
+        for(int end = str; end < n; end++){
+            for(int i = str; i <= end; i++){
+                printf("%d", arr[i]);
+            }
+            printf(" ");
+        }
+        printf("\n"); // 👈 new line after each start index
+    }
+    printf("These are the subarrays ");
+}
